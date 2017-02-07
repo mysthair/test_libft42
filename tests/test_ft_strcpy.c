9d011fedca5e67014fc51a815d3cda9c9d25f86c
jleblanc <jleblanc@student.42.fr>
2017-02-07 18:07:38 +0100

    strnequ NULL NULL

--- tests/test_ft_strcpy.c
+++ tests/test_ft_strcpy.c
@@ -4,26 +4,30 @@
 
 #include <string.h>
 
-static int test_ft_strcpy(char *dest, char *src)
+static int test_ft_strcpy(char *dest, char const *src)
 {
 	char	*t;
 	t = ft_strcpy(dest, src);
-	FAIL_IF_NOT(t == dest);
-	FAIL_IF_NOT(ft_strequ(dest, src));
+	TEST(t == dest);
+	TEST(ft_strequ(dest, src));
 	return (t == dest && ft_strequ(dest, src));
 }
 
 
 int main_test_ft_strcpy()	
 {//strcpy
-		char *abcdefgh = "abcdefgh";
+		char *s = "s";	
+		char *abcdefghi = "abcdefghi";
 		char buff100[100];
 		
 		ft_bzero(buff100, 100);
-		ft_strcpy(buff100+1, abcdefgh);
-		FAIL_IF_NOT(ft_memcmp(abcdefgh, buff100+1, 11) == 0);
 		
-		test_ft_strcpy(buff100, abcdefgh);
+		FAIL_IF_NOT(test_ft_strcpy(buff100 + 1, s));
+		FAIL_IF_NOT(test_ft_strcpy(s, s));
+		FAIL_IF_NOT(test_ft_strcpy(buff100 + 2, abcdefghi));
+		FAIL_IF_NOT(test_ft_strcpy(buff100 + 2, ""));
+		FAIL_IF_NOT(test_ft_strcpy(buff100, abcdefghi));
+		FAIL_IF_NOT(test_ft_strcpy(abcdefghi, "lolmdr\n"));
 		
 		return(1);
 }
