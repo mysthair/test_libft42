0f9a6fba6c49063cffbeafb6c2b6e95453a71bd4
jleblanc <jleblanc@student.42.fr>
2017-09-07 17:37:30 +0200

    test putstr

---
+++ tests/test_ft_putstr.c
@@ -0,0 +1,9 @@
+#include "libft.h"
+#include "tests.h"
+int main_test_ft_putstr()
+{
+  //ce test fonctionne
+  ft_putstr("[héhé Ø ça va être dur pour les élèves... Да, меня зовут Джимми] ..");
+
+  return SUCCESS;
+}
